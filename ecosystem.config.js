02e19eed06fea86fdb82bb9b944d88b9d2a323c1
girts521 <girts521@gmail.com>
2022-04-09 23:33:20 +0200

    added pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps : [{
+    name: "gkarcevskis-portfolio-api",
+    script: "./index.js",
+    instances: "2",
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    }
+  }]
+}
