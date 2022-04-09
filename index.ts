9822a2c5f427779cdd4d2b41cab40a433e23e6d7
girts521 <girts521@gmail.com>
2022-04-09 13:48:46 +0200

    minor cors mistake

--- index.ts
+++ index.ts
@@ -13,7 +13,7 @@ const app = express();
 
 //allow cors from localhost 3000
 app.use(cors({
-    origin: 'http://www.gkarcevskis.com',
+    origin: 'https://www.gkarcevskis.com',
     credentials: true
 }));
 
