fd24c15613f600106d77ca207a012c38b820968c
girts521 <girts521@gmail.com>
2022-04-10 12:31:56 +0200

    fixing pm2

---
+++ dist/index.js
@@ -0,0 +1,51 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mailgun = require("mailgun-js");
+const API_KEY = process.env.API_KEY;
+const DOMAIN = process.env.DOMAIN;
+const client = mailgun({ apiKey: API_KEY, domain: DOMAIN, host: 'api.eu.mailgun.net', protocol: 'https:' });
+const app = express();
+//allow cors from localhost 3000
+app.use(cors({
+    origin: 'https://www.gkarcevskis.com',
+    credentials: true
+}));
+//parse json
+app.use(express.json());
+const port = process.env.PORT || 3001;
+//hello world route
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+//receive messageData from portfolio and send it to mailgun
+app.post('/send', (req, res) => {
+    const { name, email, message } = req.body;
+    const data = {
+        from: `${name}, ${email}`,
+        to: 'girts@gkarcevskis.com',
+        subject: 'Portfolio message',
+        text: message
+    };
+    client.messages().send(data, function (error, body) {
+        console.log('body: ', body);
+        if (error) {
+            console.log('error: ', error);
+            res.json({
+                status: 418,
+                sent: false,
+                message: error
+            });
+        }
+        else {
+            res.json({
+                status: 200,
+                sent: true
+            });
+        }
+    });
+});
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+//# sourceMappingURL=index.js.map
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps : [{
     name: "gkarcevskis-portfolio-api",
-    script: "./index.ts",
+    script: "./dist/index.js",
     instances: "2",
     env: {
       NODE_ENV: "development",
